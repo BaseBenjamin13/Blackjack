b7c605069a30129e47126450dd42fa1375c4a307
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-03-07 17:39:42 -0500

    added users cards total value, changes color based on how good it is

--- js/game.js
+++ js/game.js
@@ -22,7 +22,11 @@
 // make ace Input a number not a string which it is now, ✅
 // Number. does not work on aceInput or im using it wrong, used parseInt found on stackoverflow ✅
 
-
+//users cards total value. ✅
+// make totalvalue var. ✅
+// make html el and put it in js. ✅
+// when hit button is clicked add random card to totalvalue. ✅
+// when total cards is below 10 then color red, above 10 color yellow, above 17 green over 21 red. ✅
 
 //dealer geting cards when pass button is clicked
 //dealer gets cards if value of cards is below users value of cards
@@ -36,11 +40,12 @@ import changeBet from "/js/betting.js";
 const hitBtn = document.querySelector("#hit-btn");
 let walletNumEl = document.getElementById("wallet-num");
 const betBtn = document.querySelector("#bet-btn");
+const usersCardsTotalValueEl = document.getElementById("cards-num");
 
 let walletNum = 5000;
 let usersCardsTotalValue = 0;
 let aceInput;
-let deckOfCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, aceInput];
+let deckOfCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, aceInput];
 
 
 //add or subtract from wallet function.
@@ -66,7 +71,7 @@ function getCards(){
     usersCards++;
     console.log({usersCards});
     let randomCard = deckOfCards[Math.floor(Math.random() * deckOfCards.length)];
-    if (randomCard == deckOfCards[13]) {
+    if (randomCard == deckOfCards[12]) {
         aceInput = prompt("You got an Ace, how do you want to use it. \n\n             11 or 1");
         //found parseInt on stackoverflow.
         aceInput = parseInt(aceInput);
@@ -74,6 +79,18 @@ function getCards(){
         // let num = aceInput + usersCards;
         // console.log(num);
     }
+    //adding total value of cards the html El.
+    usersCardsTotalValue += randomCard;
+    console.log(usersCardsTotalValue);
+    usersCardsTotalValueEl.innerText = usersCardsTotalValue;
     console.log(randomCard);
+    //changing color of total cards value.
+    if (usersCardsTotalValue > 10 && usersCardsTotalValue < 17) {
+        usersCardsTotalValueEl.style.color = 'yellow';
+    } else if (usersCardsTotalValue >= 17 && usersCardsTotalValue < 22) {
+        usersCardsTotalValueEl.style.color = 'green';
+    } else {
+        usersCardsTotalValueEl.style.color = 'red';
+    }
 }
 
