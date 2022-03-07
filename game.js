6465c478b2b5b8a8d48c7b905a410e89940922f5
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-03-06 23:09:42 -0500

    adding deck of cards array, hit button gets random card

--- game.js
+++ game.js
@@ -16,9 +16,14 @@
 // make minimum and maximum bet ✅
 // error message if min or max is not met ✅
 
-//deck of cards will be an array 
-//use math.random to get random index/card in deck
-//const deckOfCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, userInput for ace, 11 or 1];
+//deck of cards will be an array  ✅
+//use math.random to get random index/card in deck ✅
+//const deckOfCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, userInput for ace, 11 or 1]; ✅
+//if user gets ace then ask for user input to pick 11 or 1 ✅ 
+// make ace Input a number not a string which it is now,
+// Number. does not work on aceInput or im using it wrong
+
+
 
 //dealer geting cards when pass button is clicked
 //dealer gets cards if value of cards is below users value of cards
@@ -35,6 +40,10 @@ const betBtn = document.querySelector("#bet-btn");
 let walletNum = 5000;
 let minBet = 50;
 let maxBet = 500;
+let usersCardsTotalValue = 0;
+let aceInput;
+let deckOfCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, aceInput];
+
 
 //add or subtract from wallet function.
 function changeWallet(addNum, subNum) {
@@ -60,8 +69,6 @@ function changeBet(){
 }
 
 
-
-
 let usersCards = 0;
 
 //hit button, adding cards to users deck
@@ -69,4 +76,12 @@ hitBtn.addEventListener('click', getCards);
 function getCards(){
     usersCards++;
     console.log({usersCards});
+    let randomCard = deckOfCards[Math.floor(Math.random() * deckOfCards.length)];
+    if (randomCard == deckOfCards[13]) {
+        aceInput = prompt("You got an Ace, how do you want to use it. \n\n             11 or 1");
+        randomCard = aceInput;
+        // let num = aceInput + usersCards;
+        // console.log(num);
+    }
+    console.log(randomCard);
 }
