f5ebc3e966409c64a55c3f63edf771ae764cc634
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-09 19:07:06 -0500

    max bet changed to users wallet amount

--- js/betting.js
+++ js/betting.js
@@ -8,7 +8,6 @@ const currentBetEl = document.getElementById("bet-num");
 
 
 let minBet = 50;
-let maxBet = 500;
 export let betInput;
 
 
@@ -18,13 +17,13 @@ export default function changeBet(){
     if(walletNum <= 50){
         return alert("Not enough money. \n\n Add money to wallet and try again.")
     }
-    betInput = prompt("Make a bet between $50-$500");
+    betInput = prompt(`Make a bet between $50-$${walletNum}`);
     // let promptInput = prompt("make a bet between $50-$500");
     // betInput = parseInt(promptInput, 10)
     if(isNaN(betInput)){
         alert("Please enter a valid input. \n\n Input needs to be a number. \n Make a bet between $50-$500.")
     }else {
-        if (betInput >= minBet && betInput <= maxBet) {
+        if (betInput >= minBet && betInput <= walletNum) {
             changeWallet(0, betInput);
             currentBetEl.innerText = betInput;
             for (let i = 0; i < 2; i++) {
@@ -32,7 +31,7 @@ export default function changeBet(){
             }
         } else if (betInput < minBet) {
             alert("The bet is too low. \n\n\n Try Again!");
-        } else if (betInput > maxBet) {
+        } else if (betInput > walletNum) {
             alert("The bet is too high. \n\n\n Try Again!");
         }
     }
